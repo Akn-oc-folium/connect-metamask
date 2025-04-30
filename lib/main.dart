c8eccf099ec9ea1886d09a43b46ac03117f031db
Ankit <lc46377@umbc.edu>
2025-04-30 03:32:26 -0400

    data table link in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:shadcn_flutter/shadcn_flutter.dart';
+import 'package:web3_flutter/views/dashboard.dart';
 import 'package:web3_flutter/views/stock_filter_page.dart';
 
 void main() {
@@ -17,3 +18,17 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//Dashboard
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ShadcnApp(
+//       title: 'Dashboard App',
+//       theme: ThemeData(colorScheme: ColorSchemes.lightNeutral(), radius: 0.7),
+//       home: Dashboard(),
+//     );
+//   }
+// }
